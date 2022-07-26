c7967d614cee5a80e9af9e3eede9aea9022bad06
abuzarmahmood <abuzarmahmood@gmail.com>
2022-07-26 16:32:56 -0400

    autopep8

--- test_data/classify_spikes.py
+++ test_data/classify_spikes.py
@@ -3,8 +3,8 @@ import numpy as np
 from tqdm import tqdm
 import os
 import matplotlib
-#matplotlib.use('TKAgg')
-import pylab as plt 
+# matplotlib.use('TKAgg')
+import pylab as plt
 from scipy.stats import zscore
 from sklearn.decomposition import PCA as pca
 from time import time
@@ -23,12 +23,24 @@ import multiprocessing
 
 from joblib import dump, load
 
+#h5_path = '/media/bigdata/projects/neuRecommend/test_data/sorted/sorted_waveforms.h5'
+#model_save_dir = '/media/bigdata/projects/neuRecommend/'
+#path_vars = dict(
+#        h5_path = h5_path,
+#        model_save_dir = model_save_dir
+#        )
+#with open('path_vars.json','w') as path_file:
+#    json.dump(path_vars, path_file, indent = 4)
 
-h5_path = '/media/bigdata/projects/neuRecommend/test_data/sorted/sorted_waveforms.h5'
-model_save_dir = '/media/bigdata/projects/neuRecommend/'
+with open('path_vars.json','r') as path_file:
+    path_vars = json.load(path_file)
+
+h5_path = path_vars['h5_path']
+model_save_dir = path_vars['model_save_dir']
 
 def imshow(array):
-    plt.imshow(array, interpolation = 'nearest', aspect='auto')
+    plt.imshow(array, interpolation='nearest', aspect='auto')
+
 
 # Load equal numbers of waveforms for pos,neg, split into train,test
 # Since positive samples are >> negative, we will subsample from them
@@ -38,25 +50,25 @@ pos_path = '/sorted/pos'
 neg_waveforms = []
 pos_waveforms = []
 
-#with tables.open_file(h5_path,'r') as h5:
-h5 = tables.open_file(h5_path,'r')
+# with tables.open_file(h5_path,'r') as h5:
+h5 = tables.open_file(h5_path, 'r')
 for x in h5.iter_nodes(neg_path):
     neg_waveforms.append(x[:])
 
-neg_waveforms = np.concatenate(neg_waveforms,axis=0)
+neg_waveforms = np.concatenate(neg_waveforms, axis=0)
 
 # pos_waveforms needs to be of length 75, or 750 that can be downsampled
 pos_node_list = list(h5.iter_nodes(pos_path))
 # Waveforms with same length as neg_waveforms
-pos_matched_units = [x for x in pos_node_list \
-        if x.shape[1] == neg_waveforms.shape[1]]
+pos_matched_units = [x for x in pos_node_list
+                     if x.shape[1] == neg_waveforms.shape[1]]
 waveforms_per_unit = neg_waveforms.shape[0]//len(pos_matched_units)
 
-#with tables.open_file(h5_path,'r') as h5:
+# with tables.open_file(h5_path,'r') as h5:
 for x in pos_matched_units:
     ind = np.min([x.shape[0], waveforms_per_unit])
-    pos_waveforms.append(x[:ind,:])
-pos_waveforms = np.concatenate(pos_waveforms,axis=0)
+    pos_waveforms.append(x[:ind, :])
+pos_waveforms = np.concatenate(pos_waveforms, axis=0)
 h5.close()
 
 neg_label = [0]*neg_waveforms.shape[0]
@@ -64,17 +76,20 @@ pos_label = [1]*pos_waveforms.shape[0]
 fin_labels = np.concatenate([neg_label, pos_label])
 
 ############################################################
-## Train classifier
+# Train classifier
 ############################################################
-def zscore_custom(x):
-    return zscore(x,axis=-1)
-zscore_transform = FunctionTransformer(zscore_custom)
-                
 
-fin_data = np.concatenate([neg_waveforms, pos_waveforms]) 
+
+def zscore_custom(x):
+    return zscore(x, axis=-1)
+
+zscore_transform = FunctionTransformer(zscore_custom)
+
+
+fin_data = np.concatenate([neg_waveforms, pos_waveforms])
 #zscore_fin_data = zscore(fin_data, axis=-1)
 zscore_fin_data2 = zscore_transform.transform(fin_data)
-pca_obj = pca(n_components = 10).fit(zscore_fin_data2[::1000])
+pca_obj = pca(n_components=10).fit(zscore_fin_data2[::1000])
 print(f'Explained variance : {np.sum(pca_obj.explained_variance_ratio_)}')
 pca_data = pca_obj.transform(zscore_fin_data2)
 
@@ -85,24 +100,24 @@ X = X[::trim_factor]
 y = fin_labels[::trim_factor]
 
 X_train, X_test, y_train, y_test = \
-        train_test_split(X, y, test_size=0.5, random_state=1)
+    train_test_split(X, y, test_size=0.5, random_state=1)
 
 X_train, X_val, y_train, y_val = \
-        train_test_split(X_train, y_train, test_size=0.25, random_state=1) 
+    train_test_split(X_train, y_train, test_size=0.25, random_state=1)
 
 #xgb_model = xgb.XGBClassifier(n_jobs=multiprocessing.cpu_count() // 2)
-#clf = GridSearchCV(xgb_model, {'max_depth': [2, 4, 6],
+# clf = GridSearchCV(xgb_model, {'max_depth': [2, 4, 6],
 #               'n_estimators': [50, 100, 200]}, verbose=1, n_jobs=2)
 #clf.fit(X, y)
-#print(clf.best_score_)
-#print(clf.best_params_)
+# print(clf.best_score_)
+# print(clf.best_params_)
 #
-## Write out best_params to json
-optim_params_path = os.path.join(model_save_dir,'optim_params.json')
-#with open(optim_params_path,'w') as outfile:
+# Write out best_params to json
+optim_params_path = os.path.join(model_save_dir, 'optim_params.json')
+# with open(optim_params_path,'w') as outfile:
 #    json.dump(clf.best_params_, outfile, indent = 4)
 
-with open(optim_params_path,'r') as outfile:
+with open(optim_params_path, 'r') as outfile:
     best_params = json.load(outfile)
 
 #
@@ -115,11 +130,11 @@ clf.score(X_train, y_train)
 clf.score(X_test, y_test)
 
 ############################################################
-## Titrate decision boundaries 
+# Titrate decision boundaries
 ############################################################
-val_proba = clf.predict_proba(X_val)[:,1]
+val_proba = clf.predict_proba(X_val)[:, 1]
 
-all_proba = clf.predict_proba(X)[:,1]
+all_proba = clf.predict_proba(X)[:, 1]
 
 
 # Priorities in order of importance
@@ -134,20 +149,20 @@ all_proba = clf.predict_proba(X)[:,1]
 # Threshold for true positives == 99%
 wanted_thresh = 0.99
 
-thresh_vec = np.linspace(0,1,100)
+thresh_vec = np.linspace(0, 1, 100)
 true_mean_val = []
 for this_thresh in thresh_vec:
     pred_pos = val_proba >= this_thresh
-    pred_pos = pred_pos*1 
+    pred_pos = pred_pos*1
     pred_pos_mean = np.mean(pred_pos[np.where(y_val)[0]])
     true_mean_val.append(pred_pos_mean)
 true_mean_val = np.array(true_mean_val)
 
 
-false_num, proba = np.histogram(val_proba[np.where(y_val==0)[0]], bins = 100) 
+false_num, proba = np.histogram(val_proba[np.where(y_val == 0)[0]], bins=100)
 cumu_false = np.cumsum(false_num)
 scaled_cumu_false = cumu_false/np.max(cumu_false)
-        
+
 true_mean_thresh_inds = true_mean_val >= wanted_thresh
 highest_thresh = np.max(thresh_vec[true_mean_thresh_inds])
 best_false = scaled_cumu_false[np.argmin(np.abs(proba-highest_thresh))]
@@ -159,69 +174,69 @@ best_false = scaled_cumu_false[np.argmin(np.abs(proba-highest_thresh))]
 left_str = '1% Spikes' + '\n' + '93% Noise'
 right_str = '99% Spikes' + '\n' + '7% Noise'
 
-plt.hist(val_proba[np.where(y_val)[0]], bins = 50, 
-        alpha = 0.5, label = 'True Spike')
-#plt.yscale('log')
-plt.hist(val_proba[np.where(y_val==0)[0]], 
-        alpha = 0.5, bins = 50, label = 'True Noise')
+plt.hist(val_proba[np.where(y_val)[0]], bins=50,
+         alpha=0.5, label='True Spike')
+# plt.yscale('log')
+plt.hist(val_proba[np.where(y_val == 0)[0]],
+         alpha=0.5, bins=50, label='True Noise')
 plt.xlabel('Spike probability')
 plt.ylabel('Frequency (Log Scale)')
-plt.axvline(highest_thresh, 
-        color = 'red', alpha = 0.7, linewidth = 2,
-        label = f'Threshold for {wanted_thresh} Recall')
-plt.text(0.05, 0.6, left_str, transform = plt.gca().transAxes)
-plt.text(0.25, 0.6, right_str, transform = plt.gca().transAxes)
+plt.axvline(highest_thresh,
+            color='red', alpha=0.7, linewidth=2,
+            label=f'Threshold for {wanted_thresh} Recall')
+plt.text(0.05, 0.6, left_str, transform=plt.gca().transAxes)
+plt.text(0.25, 0.6, right_str, transform=plt.gca().transAxes)
 plt.title('Distribution of classification probabilities')
 plt.legend()
-plt.savefig(os.path.join(model_save_dir,'spike_classification_dist.png'),
-        dpi = 300, bbox_inches = 'tight')
+plt.savefig(os.path.join(model_save_dir, 'spike_classification_dist.png'),
+            dpi=300, bbox_inches='tight')
 plt.close()
-#plt.show()
+# plt.show()
 
 ############################################################
-# Representative plots of spikes and noise at different probs 
+# Representative plots of spikes and noise at different probs
 ############################################################
-wanted_probs = np.linspace(0,1,6)
+wanted_probs = np.linspace(0, 1, 6)
 prob_frame = pd.DataFrame(
-        dict(
-            label = y,
-            prob = all_proba 
-            )
-        )
+    dict(
+        label=y,
+        prob=all_proba
+    )
+)
 
 nrn_inds = []
 noise_inds = []
 for this_prob in wanted_probs:
     spike_val = (prob_frame['prob'][prob_frame.label == 1] - this_prob)\
-            .abs().argsort()[:10].values
+        .abs().argsort()[:10].values
     noise_val = (prob_frame['prob'][prob_frame.label == 0] - this_prob)\
-            .abs().argsort()[:10].values
+        .abs().argsort()[:10].values
     nrn_inds.append(spike_val)
     noise_inds.append(noise_val)
 
-fig,ax = plt.subplots(1,len(wanted_probs), sharey = True,
-        figsize = (13,2))
+fig, ax = plt.subplots(1, len(wanted_probs), sharey=True,
+                       figsize=(13, 2))
 for num in range(len(wanted_probs)):
-    #ax[0,num].plot(fin_data[nrn_inds[num]])
-    #ax[1,num].plot(fin_data[noise_inds[num]])
-    this_dat = zscore(fin_data[noise_inds[num]],axis=-1)
-    flip_bool = np.vectorize(np.int)(np.sign(this_dat[:,30]))
-    this_dat = np.stack([x*-1 if this_bool==1 else x \
-            for x,this_bool in zip(this_dat, flip_bool)])
+    # ax[0,num].plot(fin_data[nrn_inds[num]])
+    # ax[1,num].plot(fin_data[noise_inds[num]])
+    this_dat = zscore(fin_data[noise_inds[num]], axis=-1)
+    flip_bool = np.vectorize(np.int)(np.sign(this_dat[:, 30]))
+    this_dat = np.stack([x*-1 if this_bool == 1 else x
+                         for x, this_bool in zip(this_dat, flip_bool)])
     ax[num].plot(this_dat.T,
-            color = 'k', alpha = 0.5)
+                 color='k', alpha=0.5)
     ax[num].set_title(f'Prob : {np.round(wanted_probs[num],1)}')
     ax[num].set_xticklabels([])
     ax[num].set_xticks([])
     ax[num].set_yticklabels([])
     ax[num].set_yticks([])
-plt.savefig(os.path.join(model_save_dir,'spike_classification_examples.png'),
-        dpi = 300)
+plt.savefig(os.path.join(model_save_dir, 'spike_classification_examples.png'),
+            dpi=300)
 plt.close()
-#plt.show()
+# plt.show()
 
 ############################################################
-## Speed Test
+# Speed Test
 ############################################################
 start_t = time()
 clf.predict_proba(X)
@@ -232,8 +247,8 @@ print(end_t-start_t)
 dump(clf, os.path.join(model_save_dir, "xgb_classifier"))
 
 pipeline = Pipeline([
-                ('zscore', zscore_transform),
-                ('pca', pca_obj),
-                ('scaler', scaler_obj), 
-                ('svc', clf)])
+    ('zscore', zscore_transform),
+    ('pca', pca_obj),
+    ('scaler', scaler_obj),
+    ('svc', clf)])
 dump(pipeline, os.path.join(model_save_dir, "xgb_pipeline"))
--- test_data/recommend_electrodes.py
+++ test_data/recommend_electrodes.py
@@ -6,13 +6,24 @@ However, recommendations should err on side of more false positives if needed
 so as not to miss electrodes
 """
 
+from joblib import dump, load
+import multiprocessing
+import xgboost as xgb
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import precision_score, recall_score, confusion_matrix
+from sklearn.preprocessing import FunctionTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from ephys_data import ephys_data
 import tables
 import numpy as np
 from tqdm import tqdm
 import os
 import matplotlib
-#matplotlib.use('TKAgg')
-import pylab as plt 
+# matplotlib.use('TKAgg')
+import pylab as plt
 from scipy.stats import zscore
 from sklearn.decomposition import PCA as pca
 from time import time
@@ -21,42 +32,32 @@ import pandas as pd
 
 import sys
 sys.path.append('/media/bigdata/firing_space_plot/ephys_data')
-from ephys_data import ephys_data
 
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import FunctionTransformer
-from sklearn.metrics import precision_score, recall_score, confusion_matrix
-
-from sklearn.model_selection import GridSearchCV
-import xgboost as xgb
-import multiprocessing
-
-from joblib import dump, load
 
 model_save_dir = '/media/bigdata/projects/neuRecommend/'
 
+
 def zscore_custom(x):
-    return zscore(x,axis=-1)
+    return zscore(x, axis=-1)
+
 
 clf = load(os.path.join(model_save_dir, 'xgb_pipeline'))
 
-## Iterate over sorted recordings
+# Iterate over sorted recordings
 # Use unit_descriptors to check which electrodes have units
 waveform_file_path = '/media/bigdata/Abuzar_Data/all_spike_waveform_files.txt'
-waveform_file_list = [x.strip() for x in open(waveform_file_path,'r').readlines()]
+waveform_file_list = [x.strip()
+                      for x in open(waveform_file_path, 'r').readlines()]
 waveform_basename_list = [x.split('/')[-4] for x in waveform_file_list]
-waveform_elec_list = [re.findall('\d+',x.split('/')[-2])[0] \
-                            for x in waveform_file_list]
+waveform_elec_list = [re.findall('\d+', x.split('/')[-2])[0]
+                      for x in waveform_file_list]
 waveform_data_dir = ["/".join(x.split('/')[:-3]) for x in waveform_file_list]
 
 path_frame = pd.DataFrame({
-            'path' : waveform_file_list,
-            'basename' : waveform_basename_list,
-            'data_dir' : waveform_data_dir,
-            'electrode' : waveform_elec_list})
+    'path': waveform_file_list,
+    'basename': waveform_basename_list,
+    'data_dir': waveform_data_dir,
+    'electrode': waveform_elec_list})
 path_frame.electrode = path_frame.electrode.astype('int32')
 
 unique_frame = path_frame.drop_duplicates('data_dir')
@@ -68,41 +69,41 @@ for this_dir in unique_frame.data_dir:
     dat.get_region_units()
     pos_electrodes = np.unique([x[1] for x in dat.unit_descriptors])
     this_frame = pd.DataFrame({
-                    'data_dir' : this_dir,
-                    'electrode' : pos_electrodes,
-                    'unit' : True})
+        'data_dir': this_dir,
+        'electrode': pos_electrodes,
+        'unit': True})
     pos_frames.append(this_frame)
 
 fin_pos_frame = pd.concat(pos_frames)
 
-path_frame = path_frame.merge(fin_pos_frame, how = 'outer')
+path_frame = path_frame.merge(fin_pos_frame, how='outer')
 path_frame.fillna(False, inplace=True)
-path_frame['electrode_str'] = [f'{x:02}' for x in path_frame.electrode] 
+path_frame['electrode_str'] = [f'{x:02}' for x in path_frame.electrode]
 
 path_frame['array_name'] = path_frame.basename + '_elec' +\
-                    path_frame.electrode_str
+    path_frame.electrode_str
 
 path_frame.unit.mean()
-mean_unit_frac = path_frame.groupby('basename').agg({'unit':'mean'})['unit']
+mean_unit_frac = path_frame.groupby('basename').agg({'unit': 'mean'})['unit']
 
 cmap = plt.get_cmap('tab10')
-plt.hist(mean_unit_frac, alpha = 0.5, bins = 15, color = cmap(0));
-plt.hist(mean_unit_frac, histtype = 'step', bins = 15, color = cmap(0),
-        linewidth = 2);
-plt.axvline(np.median(mean_unit_frac), color = 'red', alpha = 0.7,
-        linewidth = 2, label = 'Median Fraction', linestyle = '--')
+plt.hist(mean_unit_frac, alpha=0.5, bins=15, color=cmap(0))
+plt.hist(mean_unit_frac, histtype='step', bins=15, color=cmap(0),
+         linewidth=2)
+plt.axvline(np.median(mean_unit_frac), color='red', alpha=0.7,
+            linewidth=2, label='Median Fraction', linestyle='--')
 plt.legend()
 plt.xlabel('Fraction of channels/dataset with neurons')
 plt.ylabel('Frequency')
-plt.xlim([0,1])
-plt.savefig(os.path.join(model_save_dir,'good_channel_frac_dist.png'),
-        dpi = 300)
+plt.xlim([0, 1])
+plt.savefig(os.path.join(model_save_dir, 'good_channel_frac_dist.png'),
+            dpi=300)
 plt.close()
-#plt.show()
+# plt.show()
 
 # For each electrode, calculate class probabilities and save in dataframe
 #path_frame['proba'] = None
-#for num, path in tqdm(enumerate(path_frame.path)):
+# for num, path in tqdm(enumerate(path_frame.path)):
 #    x = np.load(path)
 #    if x.shape[1] == 75:
 #        proba = clf.predict_proba(x)
@@ -114,22 +115,22 @@ plt.close()
 #    path_frame['proba'].loc[num] = proba
 
 # Save frame to avoid having to calculate proba again
-#path_frame.to_pickle(os.path.join(model_save_dir,'path_frame.pkl'))
-path_frame = pd.read_pickle(os.path.join(model_save_dir,'path_frame.pkl'))
+# path_frame.to_pickle(os.path.join(model_save_dir,'path_frame.pkl'))
+path_frame = pd.read_pickle(os.path.join(model_save_dir, 'path_frame.pkl'))
 
-path_frame['wave_count'] = [x.shape[0] for x in path_frame['proba']] 
+path_frame['wave_count'] = [x.shape[0] for x in path_frame['proba']]
 
 # We want atleast 2000 waveforms to want to look at an electrodes
 # Given that, we want to find the best precision/recall combo
 unit_vec = path_frame.unit.to_numpy()
 proba_vec = path_frame.proba.to_numpy()
 
-fin_proba_vec = [x[:,1] for x in proba_vec]
-pred_spike_count = [np.sum(x>0.18) for x in fin_proba_vec]
+fin_proba_vec = [x[:, 1] for x in proba_vec]
+pred_spike_count = [np.sum(x > 0.18) for x in fin_proba_vec]
 rec_channel_vec = np.array(pred_spike_count) > 2000
-#precision = precision_score(unit_vec, rec_list) 
-recall = recall_score(unit_vec, rec_channel_vec) 
+#precision = precision_score(unit_vec, rec_list)
+recall = recall_score(unit_vec, rec_channel_vec)
 confusion_matrix(unit_vec, rec_channel_vec, normalize='all')
 
 # Given no neuron, how many channels did we correctly throw out
-np.mean(rec_channel_vec[unit_vec==False] == False)
+np.mean(rec_channel_vec[unit_vec == False] == False)
