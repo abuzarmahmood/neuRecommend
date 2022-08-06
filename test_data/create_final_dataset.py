2b1df6ff43d98b60145993524d821ea3368c2236
abuzarmahmood <abuzarmahmood@gmail.com>
2022-08-06 00:06:58 -0400

    Add writing of metadata

--- test_data/create_final_dataset.py
+++ test_data/create_final_dataset.py
@@ -8,11 +8,12 @@ import numpy as np
 from tqdm import tqdm
 import os
 import json
+import pandas as pd
 
 with open('path_vars.json','r') as path_file:
     path_vars = json.load(path_file)
 
-h5_path = path_vars['h5_path']
+h5_path = path_vars['h5_orig_path']
 h5_dir = os.path.dirname(h5_path)
 model_save_dir = path_vars['model_save_dir']
 
@@ -44,16 +45,24 @@ hf5_out = tables.open_file(os.path.join(h5_dir, 'final_dataset.h5')
 hf5_out.create_group('/sorted', 'pos', createparents=True)
 hf5_out.create_group('/sorted', 'neg', createparents=True)
 
+# Also create a pandas dataframe to record metadata
+metadata_keys = ['name','waveform_count','timebins','class']
+metadata_list = []
+
 for this_node in tqdm(pos_node_list):
     save_path = pos_path
     if this_node.shape[1] == 750:
         trim = 10
     else:
         trim = 1
+    fin_data = this_node[:waveforms_per_unit, ::trim]
     hf5_out.create_array(
             save_path,
             this_node.name,
-            this_node[:waveforms_per_unit, ::trim]
+            fin_data 
+            )
+    metadata_list.append(
+            [this_node.name, *fin_data.shape, 'pos']
             )
 
 for this_node in tqdm(neg_node_list):
@@ -62,11 +71,35 @@ for this_node in tqdm(neg_node_list):
         trim = 10
     else:
         trim = 1
+    fin_data = this_node[:waveforms_per_unit, ::trim]
     hf5_out.create_array(
             save_path,
             this_node.name,
-            this_node[:,::trim]
+            fin_data
+            )
+    metadata_list.append(
+            [this_node.name, *fin_data.shape, 'neg']
             )
 
 hf5_out.flush()
 hf5_out.close()
+h5.flush()
+h5.close()
+
+metadata_frame = pd.DataFrame(
+        data = metadata_list,
+        columns = metadata_keys
+        )
+
+metadata_frame.to_csv(os.path.join(h5_dir, 'fin_data_metadata.csv'))
+
+summary_dat = dict(
+        cluster_counts = metadata_frame.groupby('class')\
+                .count().iloc[:,0].to_dict(),
+        summary_stats = str(metadata_frame.groupby('class').describe().T)
+        )
+summary_dat_str = str(summary_dat)
+
+with open(os.path.join(h5_dir, 'fin_data_summary_stats.json'), 'w') as out_file:
+    #json.dump(summary_dat, out_file,  indent = 4)
+    out_file.writelines(summary_dat_str)
