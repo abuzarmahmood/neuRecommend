1a4d1175dc3a3549b3b1f52430317f801cfb490b
abuzarmahmood <abuzarmahmood@gmail.com>
2023-03-20 12:45:29 -0400

    Add autoencoder as a class with methods appropriate for the pipeline

--- src/_experimental/dim_reduction_comparison/compare_methods.py
+++ src/_experimental/dim_reduction_comparison/compare_methods.py
@@ -9,9 +9,82 @@ from sklearn.decomposition import PCA
 import umap
 import sys
 from time import time
-from tqdm import tqdm, trange
+from tqdm import trange
 load_data_path = '/media/bigdata/projects/neuRecommend/src/create_pipeline'
 sys.path.append(load_data_path)
+
+
+class autoencoder_class(self):
+    """
+    Autoencoder constructed from sklearn.neural_network.MLPRegressor
+    """
+
+    def __init__(self, input_shape):
+        self.input_shape = input_shape
+        self.model = self.create_model(input_shape)
+
+    def create_model(self, input_shape):
+        """
+        Generate autoencoder according to input shape and save
+        both autoencoder and encoder models
+        """
+        # Define the input layer
+        input_layer = Input(shape=(input_shape,))
+
+        # Define the encoder
+        encoded = Dense(500, activation='relu')(input_layer)
+        encoded = Dense(250, activation='relu')(encoded)
+        encoded = Dense(125, activation='relu')(encoded)
+        encoded = Dense(50, activation='relu')(encoded)
+        encoded = Dense(2, activation='relu')(encoded)
+
+        # Define the decoder
+        decoded = Dense(50, activation='relu')(encoded)
+        decoded = Dense(125, activation='relu')(decoded)
+        decoded = Dense(250, activation='relu')(decoded)
+        decoded = Dense(500, activation='relu')(decoded)
+        decoded = Dense(input_shape, activation='relu')(decoded)
+
+        # Define the model
+        autoencoder = Model(input_layer, decoded)
+
+        # Compile the model
+        autoencoder.compile(optimizer='adam', loss='mse')
+
+        # Define the encoder model
+        encoder = Model(input_layer, encoded)
+
+        self.autoencoder = autoencoder
+        self.encoder = encoder
+
+    def fit(self,
+            data,
+            epochs=100,
+            batch_size=256,
+            shuffle=True,
+            validation_split=0.2):
+        """
+        Fit the autoencoder model to the data
+        """
+        self.autoencoder.fit(data, data,
+                             epochs=epochs,
+                             batch_size=batch_size,
+                             shuffle=shuffle,
+                             validation_split=validation_split,
+                             callbacks=[EarlyStopping(monitor='val_loss', patience=10)])
+
+    def transform(self, data):
+        """
+        Transform the data using the encoder model
+        """
+        return self.encoder.predict(data)
+
+    def inverse_transform(self, data):
+        """
+        Recreate the data using the autoencoder model
+        """
+        return self.autoencoder.predict(data)
+
 
 class dim_red_comparison():
     """
@@ -27,6 +100,12 @@ class dim_red_comparison():
     """
 
     def __init__(self, data, labels):
+        """
+        Initialize the class with the data and labels
+
+        data: numpy array of shape (n_samples, n_features)
+        labels: numpy array of shape (n_samples, )
+        """
         self.data = data
         self.labels = labels
 
@@ -45,43 +124,8 @@ class dim_red_comparison():
         Autoencoder constructed from sklearn.neural_network.MLPRegressor
         """
         print(f'Creating Autoencoder with {n_components} components')
-        # Define the input layer
-        input_layer = Input(shape=(self.data.shape[1],))
-
-        # Define the encoder
-        encoded = Dense(1000, activation='relu')(input_layer)
-        encoded = Dense(500, activation='relu')(encoded)
-        encoded = Dense(250, activation='relu')(encoded)
-        encoded = Dense(125, activation='relu')(encoded)
-        encoded = Dense(50, activation='relu')(encoded)
-        encoded = Dense(n_components, activation='relu')(encoded)
-
-        # Define the decoder
-        decoded = Dense(50, activation='relu')(encoded)
-        decoded = Dense(125, activation='relu')(decoded)
-        decoded = Dense(250, activation='relu')(decoded)
-        decoded = Dense(500, activation='relu')(decoded)
-        decoded = Dense(1000, activation='relu')(decoded)
-        decoded = Dense(self.data.shape[1], activation='relu')(decoded)
-
-        # Define the model
-        autoencoder = Model(input_layer, decoded)
-
-        # Compile the model
-        autoencoder.compile(optimizer='adam', loss='mse')
-
-        # Define the encoder model
-        encoder = Model(input_layer, encoded)
-
-        # Fit the model
-        autoencoder.fit(self.data, self.data,
-                        epochs=100,
-                        batch_size=256,
-                        shuffle=True,
-                        validation_split=0.2,
-                        callbacks=[EarlyStopping(monitor='val_loss', patience=10)])
-
-        return encoder
+        autoencoder = autoencoder_class(self.data.shape[1])
+        return autoencoder
 
     def umap(self, n_components=2):
         """
@@ -122,7 +166,11 @@ class dim_red_comparison():
         print(f'Reconstruct data: {fitted_model}')
         return fitted_model.inverse_transform(transformed_data)
 
-    def calculate_variance_explained(self, original_data, transformed_data, fitted_model):
+    def calculate_variance_explained(
+            self,
+            original_data,
+            transformed_data,
+            fitted_model):
         """
         Calculate the variance explained by the transformed data
         """
@@ -146,12 +194,16 @@ class dim_red_comparison():
         fitted_model, fit_time = self.fit_method(model)
         transformed_data, transform_time = self.transform_method(fitted_model)
         variance = self.calculate_variance_explained(
-                    self.data, 
-                    self.reconstruct_data(fitted_model, transformed_data),
-                    fitted_model)
+            self.data,
+            self.reconstruct_data(fitted_model, transformed_data),
+            fitted_model)
         return fit_time, transform_time, variance
 
-    def iterate_over_components(self, min_components, max_components, model_name):
+    def iterate_over_components(
+            self,
+            min_components,
+            max_components,
+            model_name):
         """
         Run <run_methods_for_n_components> for all the components in the range
         """
@@ -159,8 +211,13 @@ class dim_red_comparison():
         transform_times = []
         variance_explained = []
         for n_components in trange(min_components, max_components):
-            fit_time, transform_time, variance = self.run_methods_for_n_components(
-                n_components, model_name)
+            (
+                fit_time,
+                transform_time,
+                variance) = self.run_methods_for_n_components(
+                n_components,
+                model_name
+            )
             fit_times.append(fit_time)
             transform_times.append(transform_time)
             variance_explained.append(variance)
